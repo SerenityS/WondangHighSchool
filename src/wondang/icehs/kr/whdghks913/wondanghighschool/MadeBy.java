3008b0fd0750e51b6b1943c05d02cfeb9852c1bd
whdghks913 <whdghks913@naver.com>
2014-03-09 17:57:27 +0900

    만든이 정보 Crouton

--- src/wondang/icehs/kr/whdghks913/wondanghighschool/MadeBy.java
+++ src/wondang/icehs/kr/whdghks913/wondanghighschool/MadeBy.java
@@ -1,14 +1,23 @@
 package wondang.icehs.kr.whdghks913.wondanghighschool;
 
+import com.tistory.whdghks913.croutonhelper.CroutonHelper;
+
 import android.os.Bundle;
 import android.app.Activity;
 
 public class MadeBy extends Activity {
 
+	private CroutonHelper mHelper;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_made_by);
+
+		mHelper = new CroutonHelper(this);
+		
+		mHelper.setText("이 앱을 만든이 정보입니다");
+		mHelper.show();
 	}
 
 }
