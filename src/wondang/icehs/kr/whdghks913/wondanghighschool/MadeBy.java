84ab9c80e5be5737eef03c7b71bc6720ebd1fc7c
whdghks913 <whdghks913@naver.com>
2014-03-09 20:39:44 +0900

    라이센스 정보 추가

--- src/wondang/icehs/kr/whdghks913/wondanghighschool/MadeBy.java
+++ src/wondang/icehs/kr/whdghks913/wondanghighschool/MadeBy.java
@@ -1,9 +1,14 @@
 package wondang.icehs.kr.whdghks913.wondanghighschool;
 
-import com.tistory.whdghks913.croutonhelper.CroutonHelper;
-
-import android.os.Bundle;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+import com.tistory.whdghks913.croutonhelper.CroutonHelper;
 
 public class MadeBy extends Activity {
 
@@ -18,6 +23,35 @@ public class MadeBy extends Activity {
 
 		mHelper.setText("이 앱을 만든이 정보입니다");
 		mHelper.show();
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.madeby, menu);
+
+		return super.onCreateOptionsMenu(menu);
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		int ItemId = item.getItemId();
+
+		if (ItemId == R.id.license) {
+			AlertDialog.Builder alert = new AlertDialog.Builder(this);
+			alert.setTitle("라이센스 정보");
+			alert.setPositiveButton("확인",
+					new DialogInterface.OnClickListener() {
+						@Override
+						public void onClick(DialogInterface dialog, int which) {
+							dialog.dismiss();
+						}
+					});
+			alert.setMessage(R.string.license);
+			alert.show();
+		}
+
+		return super.onOptionsItemSelected(item);
 	}
 
 }
