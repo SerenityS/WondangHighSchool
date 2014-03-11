f23b31ed680f4a6fce236d5a5fcd2b63a010cc72
whdghks913 <whdghks913@naver.com>
2014-03-11 18:37:30 +0900

    웹사이트 주소 변경

--- src/wondang/icehs/kr/whdghks913/wondanghighschool/WondangInfo.java
+++ src/wondang/icehs/kr/whdghks913/wondanghighschool/WondangInfo.java
@@ -35,8 +35,7 @@ public class WondangInfo extends SherlockActivity {
 
 		if (ItemId == R.id.goWeb) {
 			Intent mIntent = new Intent(this, Webview.class);
-			mIntent.putExtra("url",
-					"http://wondang.icehs.kr/");
+			mIntent.putExtra("url", "http://wondang.icehs.kr/sub/info.do?m=0101&s=wondang");
 			startActivity(mIntent);
 		} else if (ItemId == R.id.infoVideo) {
 			LayoutInflater inflater = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
