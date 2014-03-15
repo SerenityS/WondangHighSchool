dec820125e0f17ff40ce77fb2bf6df07ae5484c9
whdghks913 <whdghks913@naver.com>
2014-03-15 18:23:33 +0900

    어플 업데이트시 마다 DB업데이트

--- src/wondang/icehs/kr/whdghks913/wondanghighschool/Schedule.java
+++ src/wondang/icehs/kr/whdghks913/wondanghighschool/Schedule.java
@@ -3,13 +3,14 @@ package wondang.icehs.kr.whdghks913.wondanghighschool;
 import java.lang.ref.WeakReference;
 import java.util.Calendar;
 
-import com.tistory.whdghks913.croutonhelper.CroutonHelper;
-
-import de.keyboardsurfer.android.widget.crouton.Style;
-
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.content.SharedPreferences;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.NameNotFoundException;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
@@ -17,6 +18,10 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.widget.ListView;
 import android.widget.TextView;
+
+import com.tistory.whdghks913.croutonhelper.CroutonHelper;
+
+import de.keyboardsurfer.android.widget.crouton.Style;
 
 public class Schedule extends Activity {
 	private final String loadingList = "데이터를 가져오고 있습니다..";
@@ -32,7 +37,7 @@ public class Schedule extends Activity {
 	private ProgressDialog mDialog;
 
 	private Calendar mCalendar;
-	private SharedPreferences ScheduleList;
+	private SharedPreferences ScheduleList, Info;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -47,17 +52,30 @@ public class Schedule extends Activity {
 
 		mCalendar = Calendar.getInstance();
 
+		Info = getSharedPreferences("Info", 0);
 		ScheduleList = getSharedPreferences("March", 0);
-		if (ScheduleList.getInt("days", 0) == 0) {
-			// 데이터 복사
-			PreferenceData mData = new PreferenceData();
 
-			mData.copyDB(this, getPackageName(), "March.xml", true);
-			mData.copyDB(this, getPackageName(), "April.xml", true);
-			mData.copyDB(this, getPackageName(), "May.xml", true);
-			mData.copyDB(this, getPackageName(), "June.xml", true);
-			mData.copyDB(this, getPackageName(), "July.xml", true);
-			mData.copyDB(this, getPackageName(), "August.xml", true);
+		try {
+			PackageManager packageManager = this.getPackageManager();
+			PackageInfo infor = packageManager.getPackageInfo(getPackageName(),
+					PackageManager.GET_META_DATA);
+			final int code = infor.versionCode;
+
+			if (Info.getInt("update_code", 0) != code
+					|| ScheduleList.getInt("days", 0) == 0) {
+				PreferenceData mData = new PreferenceData();
+
+				mData.copyDB(this, getPackageName(), "March.xml", true);
+				mData.copyDB(this, getPackageName(), "April.xml", true);
+				mData.copyDB(this, getPackageName(), "May.xml", true);
+				mData.copyDB(this, getPackageName(), "June.xml", true);
+				mData.copyDB(this, getPackageName(), "July.xml", true);
+				mData.copyDB(this, getPackageName(), "August.xml", true);
+
+				Info.edit().putInt("update_code", code).commit();
+			}
+		} catch (NameNotFoundException e) {
+			e.printStackTrace();
 		}
 
 		sync();
