36cc27bb4d9620e5aabeb86d45c872ca8e957d61
whdghks913 <whdghks913@hanmail.net>
2014-08-28 22:33:51 +0900

    오늘 날짜로 이동하는 메뉴 추가, 타이틀바 홈버튼 활성화

--- src/wondang/icehs/kr/whdghks913/wondanghighschool/timetable/TimeTableScrollTab.java
+++ src/wondang/icehs/kr/whdghks913/wondanghighschool/timetable/TimeTableScrollTab.java
@@ -3,14 +3,19 @@ package wondang.icehs.kr.whdghks913.wondanghighschool.timetable;
 import java.util.Calendar;
 
 import wondang.icehs.kr.whdghks913.wondanghighschool.R;
+import android.annotation.SuppressLint;
+import android.app.ActionBar;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class TimeTableScrollTab extends FragmentActivity {
 	public final String[] DAY = { "월요일", "화요일", "수요일", "목요일", "금요일" };
@@ -20,6 +25,7 @@ public class TimeTableScrollTab extends FragmentActivity {
 
 	String mFileName, tableName;
 
+	@SuppressLint("NewApi")
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -34,6 +40,12 @@ public class TimeTableScrollTab extends FragmentActivity {
 
 		mViewPager = (ViewPager) findViewById(R.id.pager);
 		mViewPager.setAdapter(mSectionsPagerAdapter);
+
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH) {
+			ActionBar actionBar = getActionBar();
+			actionBar.setHomeButtonEnabled(true);
+			actionBar.setDisplayHomeAsUpEnabled(true);
+		}
 	}
 
 	@Override
@@ -41,14 +53,7 @@ public class TimeTableScrollTab extends FragmentActivity {
 		super.onResume();
 
 		if (mViewPager.getChildCount() > 0) {
-			Calendar calendar = Calendar.getInstance();
-			int dayOfWeek = calendar.get(Calendar.DAY_OF_WEEK);
-
-			if (dayOfWeek > 1 && dayOfWeek < 7) {
-				mViewPager.setCurrentItem(dayOfWeek - 2);
-			} else {
-				mViewPager.setCurrentItem(0);
-			}
+			setCurrentItem();
 		}
 	}
 
@@ -74,6 +79,34 @@ public class TimeTableScrollTab extends FragmentActivity {
 		public CharSequence getPageTitle(int position) {
 			return DAY[position];
 		}
+	}
+
+	private void setCurrentItem() {
+		Calendar calendar = Calendar.getInstance();
+		int dayOfWeek = calendar.get(Calendar.DAY_OF_WEEK);
+
+		if (dayOfWeek > 1 && dayOfWeek < 7) {
+			mViewPager.setCurrentItem(dayOfWeek - 2);
+		} else {
+			mViewPager.setCurrentItem(0);
+		}
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.timetable, menu);
+		return super.onCreateOptionsMenu(menu);
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		int id = item.getItemId();
+
+		if (id == R.id.today) {
+			setCurrentItem();
+		}
+
+		return super.onOptionsItemSelected(item);
 	}
 
 }
