a89363c07e95af0e011732c3d461208f9fa59e58
whdghks913 <whdghks913@hanmail.net>
2014-08-28 17:51:01 +0900

    TimeTableClass를 TimeTableGrade로 통합

--- src/wondang/icehs/kr/whdghks913/wondanghighschool/timetable/TimeTableGrade.java
+++ src/wondang/icehs/kr/whdghks913/wondanghighschool/timetable/TimeTableGrade.java
@@ -1,8 +1,6 @@
 package wondang.icehs.kr.whdghks913.wondanghighschool.timetable;
 
 import wondang.icehs.kr.whdghks913.wondanghighschool.R;
-import wondang.icehs.kr.whdghks913.wondanghighschool.rss.RssFull;
-import wondang.icehs.kr.whdghks913.wondanghighschool.rss.RssInfo;
 import android.annotation.SuppressLint;
 import android.annotation.TargetApi;
 import android.app.ActionBar;
@@ -12,7 +10,13 @@ import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class TimeTableGrade extends FragmentActivity implements
 		ActionBar.OnNavigationListener {
@@ -65,12 +69,56 @@ public class TimeTableGrade extends FragmentActivity implements
 	}
 
 	private Fragment getFragmentView(Context mContext, int position) {
-		return new TimeTableClass(mContext, Integer.toString(position + 1));
+		return new TimeTableClass(mContext, position + 1);
 	}
 
 	@Override
 	public void onConfigurationChanged(Configuration newConfig) {
 		super.onConfigurationChanged(newConfig);
+	}
+
+	@SuppressLint("ValidFragment")
+	public class TimeTableClass extends Fragment {
+		private Context mContext;
+		private int Grade;
+
+		private ListView mListView;
+		private ListViewAdapter mAdapter;
+
+		public TimeTableClass(Context mContext, int Grade) {
+			this.mContext = mContext;
+			this.Grade = Grade;
+		}
+
+		@SuppressLint("NewApi")
+		@Override
+		public View onCreateView(LayoutInflater inflater, ViewGroup container,
+				Bundle savedInstanceState) {
+			View view = inflater.inflate(R.layout.activity_rss_fullrss, null);
+
+			mListView = (ListView) view.findViewById(R.id.mListView);
+
+			mAdapter = new ListViewAdapter(mContext);
+			mListView.setAdapter(mAdapter);
+
+			mListView.setOnItemClickListener(new OnItemClickListener() {
+
+				@Override
+				public void onItemClick(AdapterView<?> parent, View v,
+						int position, long id) {
+					String dbName = mAdapter.mListData.get(position).dbName;
+					String tableName = mAdapter.mListData.get(position).tableName;
+				}
+			});
+
+			for (int i = 1; i <= 10; i++) {
+				mAdapter.addItem(Grade + "학년 " + i + "반", "WondangTimeTableG"
+						+ Grade, "grade" + Grade + "class" + i);
+			}
+			// mAdapter.addItem(null, "교무실 (대표전화)", "032-569-0723");
+
+			return view;
+		}
 	}
 
 }
