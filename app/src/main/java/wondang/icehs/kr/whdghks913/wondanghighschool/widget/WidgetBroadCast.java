8e0882a543c5be6075506971888ad34d6f20838b
whdghks913 <whdghks913@naver.com>
2015-02-25 11:51:22 +0900

    DATE CHANGED Widget Update

--- app/src/main/java/wondang/icehs/kr/whdghks913/wondanghighschool/widget/WidgetBroadCast.java
+++ app/src/main/java/wondang/icehs/kr/whdghks913/wondanghighschool/widget/WidgetBroadCast.java
@@ -1,9 +1,13 @@
 package wondang.icehs.kr.whdghks913.wondanghighschool.widget;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.widget.Toast;
+
+import java.util.Calendar;
 
 import wondang.icehs.kr.whdghks913.wondanghighschool.R;
 
@@ -13,11 +17,17 @@ public class WidgetBroadCast extends BroadcastReceiver {
     public void onReceive(Context mContext, Intent mIntent) {
         String ACTION = mIntent.getAction();
 
-        if (Intent.ACTION_DATE_CHANGED.equals(ACTION)) {
-            WidgetTool.updateWidgetData(mContext);
-        } else if ("itmir.tistory.com.UPDATE_ACTION".equals(ACTION)) {
-            Toast.makeText(mContext, R.string.widget_updating, Toast.LENGTH_SHORT).show();
-            WidgetTool.updateWidgetData(mContext);
+        Toast.makeText(mContext, R.string.widget_updating, Toast.LENGTH_SHORT).show();
+        WidgetTool.updateWidgetData(mContext);
+
+        if (Intent.ACTION_BOOT_COMPLETED.equals(ACTION)) {
+            Calendar mCalendar = Calendar.getInstance();
+            AlarmManager mAlarm = (AlarmManager) mContext.getSystemService(Context.ALARM_SERVICE);
+
+            Intent mIntentDate = new Intent(mContext, WidgetBroadCast.class).setAction("itmir.tistory.com.UPDATE_ACTION");
+            PendingIntent mPending = PendingIntent.getBroadcast(mContext, 0, mIntentDate, 0);
+            mCalendar.set(mCalendar.get(Calendar.YEAR), mCalendar.get(Calendar.MONTH), mCalendar.get(Calendar.DAY_OF_MONTH) + 1, 0, 0);
+            mAlarm.setRepeating(AlarmManager.RTC_WAKEUP, mCalendar.getTimeInMillis(), 24 * 60 * 60 * 1000, mPending);
         }
     }
 }
