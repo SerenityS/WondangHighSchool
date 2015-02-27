df6806e43c62853e8c381b43c687ffee05ae41ef
whdghks913 <whdghks913@naver.com>
2015-02-27 10:31:36 +0900

    2015년 학사일정 업데이트

--- app/src/main/java/wondang/icehs/kr/whdghks913/wondanghighschool/schedule/ScheduleActivity.java
+++ app/src/main/java/wondang/icehs/kr/whdghks913/wondanghighschool/schedule/ScheduleActivity.java
@@ -70,97 +70,88 @@ public class ScheduleActivity extends ActionBarActivity {
         });
 
         mItem.add(new SectionItem("2015년 3월 일정"));
-        mItem.add(new EntryItem("3.1절", "2015.03.01 (일)"));
-        mItem.add(new EntryItem("2015년 일정은 아직 준비중입니다", "2015.03.02 (월)"));
+        mItem.add(new EntryItem("3.1절", "2015.03.01 (일)", true));
+        mItem.add(new EntryItem("입학식 및 시업식", "2015.03.02 (월)"));
+        mItem.add(new EntryItem("전국 연합 학력 평가 (전학년)", "2015.03.11 (수)"));
+        mItem.add(new EntryItem("학부모 총회", "2015.03.13 (금)"));
 
-//        mItem.add(new SectionItem("2014년 3월 일정"));
-//        mItem.add(new EntryItem("3.1절", "2014.03.01 (토)", true));
-//        mItem.add(new EntryItem("입학식", "2014.03.03 (월)"));
-//        mItem.add(new EntryItem("전국 연합 학력 평가 (전학년)", "2014.03.12 (수)"));
-//        mItem.add(new EntryItem("학부모 총회", "2014.03.14 (금)"));
-//        mItem.add(new EntryItem("현장 학습 (1~2학년)", "2014.03.26 (수)"));
-//        mItem.add(new EntryItem("현장 학습 (1~2학년)", "2014.03.27 (목)"));
-//        mItem.add(new EntryItem("현장 학습 (1~2학년)", "2014.03.28 (금)"));
-//
-//        mItem.add(new SectionItem("2014년 4월 일정"));
-//        mItem.add(new EntryItem("전국 연합 학력 평가 (3학년)", "2014.04.10 (목)"));
-//        mItem.add(new EntryItem("영어 듣기 평가 (1학년)", "2014.04.15 (화)"));
-//        mItem.add(new EntryItem("영어 듣기 평가 (2학년)", "2014.04.16 (수)"));
-//        mItem.add(new EntryItem("영어 듣기 평가 (3학년)", "2014.04.17 (목)"));
-//        mItem.add(new EntryItem("1학기 1회고사", "2014.04.28 (월)"));
-//        mItem.add(new EntryItem("1학기 1회고사", "2014.04.29 (화)"));
-//        mItem.add(new EntryItem("1학기 1회고사", "2014.04.30 (수)"));
-//
-//        mItem.add(new SectionItem("2014년 5월 일정"));
-//        mItem.add(new EntryItem("어린이날", "2014.05.05 (월)", true));
-//        mItem.add(new EntryItem("석가탄신일", "2014.05.06 (화)", true));
-//        mItem.add(new EntryItem("교내체육대회(1학년, 2학년) / 현장학습(3학년)",
-//                "2014.05.09 (금)"));
-//
-//        mItem.add(new SectionItem("2014년 6월 일정"));
-//        mItem.add(new EntryItem("지방 선거", "2014.06.04 (수)", true));
-//        mItem.add(new EntryItem("개교 기념일", "2014.06.05 (목)", true));
-//        mItem.add(new EntryItem("현충일", "2014.06.06 (금)", true));
-//        mItem.add(new EntryItem("대수능모의평가(3학년) / 전국연합학력평가(1학년, 2학년)",
-//                "2014.06.12 (목)"));
-//        mItem.add(new EntryItem("국가 수준 학업 성취도 평가 (2학년)", "2014.06.24 (화)"));
-//
-//        mItem.add(new SectionItem("2014년 7월 일정"));
-//        mItem.add(new EntryItem("1학기 2회고사", "2014.07.04 (금)"));
-//        mItem.add(new EntryItem("1학기 2회고사", "2014.07.07 (월)"));
-//        mItem.add(new EntryItem("1학기 2회고사", "2014.07.08 (화)"));
-//        mItem.add(new EntryItem("1학기 2회고사", "2014.07.09 (수)"));
-//        mItem.add(new EntryItem("전국 연합 학력 평가 (3학년)", "2014.07.10 (목)"));
-//        mItem.add(new EntryItem("여름방학식", "2014.07.21 (월)", true));
-//
-//        mItem.add(new SectionItem("2014년 8월 일정"));
-//        mItem.add(new EntryItem("개학식", "2014.08.18 (월)"));
-//
-//        mItem.add(new SectionItem("2014년 9월 일정"));
-//        mItem.add(new EntryItem("대수능모의평가(3학년) / 전국연합학력평가(1학년, 2학년)",
-//                "2014.09.03 (수)"));
-//        mItem.add(new EntryItem("추석 연휴", "2014.09.07 (일)", true));
-//        mItem.add(new EntryItem("추석", "2014.09.08 (월)", true));
-//        mItem.add(new EntryItem("추석 연휴", "2014.09.09 (화)", true));
-//        mItem.add(new EntryItem("추석 연휴", "2014.09.10 (수)", true));
-//        mItem.add(new EntryItem("영어 듣기 능력 평가 (1학년)", "2014.09.16 (화)"));
-//        mItem.add(new EntryItem("영어 듣기 능력 평가 (2학년)", "2014.09.17 (수)"));
-//        mItem.add(new EntryItem("영어 듣기 능력 평가 (3학년)", "2014.09.18 (목)"));
-//
-//        mItem.add(new SectionItem("2014년 10월 일정"));
-//        mItem.add(new EntryItem("개천절", "2014.10.03 (금)", true));
-//        mItem.add(new EntryItem("전국 연합 학력 평가(3학년)", "2014.10.07 (화)"));
-//        mItem.add(new EntryItem("한글날", "2014.10.09 (목)", true));
-//        mItem.add(new EntryItem("2학기 1회고사", "2014.10.13 (월)"));
-//        mItem.add(new EntryItem("2학기 1회고사", "2014.10.14 (화)"));
-//        mItem.add(new EntryItem("2학기 1회고사", "2014.10.15 (수)"));
-//
-//        mItem.add(new SectionItem("2014년 11월 일정"));
-//        mItem.add(new EntryItem("대학 수학 능력 시험 (수능)", "2014.11.13 (목)"));
-//        mItem.add(new EntryItem("2학기 2회고사(3학년)", "2014.11.17 (월)"));
-//        mItem.add(new EntryItem("2학기 2회고사(3학년) / 전국 연합 학력 평가 (1학년, 2학년)",
-//                "2014.11.18 (화)"));
-//        mItem.add(new EntryItem("2학기 2회고사(3학년)", "2014.11.19 (수)"));
-//        mItem.add(new EntryItem("2학기 2회고사(3학년)", "2014.11.20 (목)"));
-//
-//        mItem.add(new SectionItem("2014년 12월 일정"));
-//        mItem.add(new EntryItem("졸업사정회", "2014.12.08 (월)"));
-//        mItem.add(new EntryItem("2학기 2회고사(1학년, 2학년)", "2014.12.15 (월)"));
-//        mItem.add(new EntryItem("2학기 2회고사(1학년, 2학년)", "2014.12.16 (화)"));
-//        mItem.add(new EntryItem("2학기 2회고사(1학년, 2학년)", "2014.12.17 (수)"));
-//        mItem.add(new EntryItem("2학기 2회고사(1학년, 2학년)", "2014.12.18 (목)"));
-//        mItem.add(new EntryItem("한마음 으뜸제", "2014.12.29 (월)"));
-//        mItem.add(new EntryItem("겨울 방학식", "2014.12.31 (수)", true));
-//
-//        mItem.add(new SectionItem("2015년 1월 일정"));
-//        mItem.add(new EntryItem("신정", "2015.01.01 (목)", true));
-//
-//        mItem.add(new SectionItem("2015년 2월 일정"));
-//        mItem.add(new EntryItem("개학식", "2015.02.06 (금)"));
-//        mItem.add(new EntryItem("졸업식 / 종업식", "2015.02.13 (금)", true));
-//        mItem.add(new EntryItem("설 연휴", "2015.02.18 (수)", true));
-//        mItem.add(new EntryItem("설", "2015.02.19 (목)", true));
-//        mItem.add(new EntryItem("설 연휴", "2015.02.20 (금)", true));
+        mItem.add(new SectionItem("2015년 4월 일정"));
+        mItem.add(new EntryItem("전국 연합 학력 평가 (3학년)", "2015.04.09 (목)"));
+        mItem.add(new EntryItem("영어 듣기 평가", "2015.04.14 (화)"));
+        mItem.add(new EntryItem("영어 듣기 평가", "2015.04.15 (수)"));
+        mItem.add(new EntryItem("영어 듣기 평가", "2015.04.16 (목)"));
+        mItem.add(new EntryItem("1학기 1회고사", "2015.04.27 (월)"));
+        mItem.add(new EntryItem("1학기 1회고사", "2015.04.28 (화)"));
+        mItem.add(new EntryItem("1학기 1회고사", "2015.04.29 (수)"));
+        mItem.add(new EntryItem("1학기 1회고사", "2015.04.30 (목)"));
+
+        mItem.add(new SectionItem("2015년 5월 일정"));
+        mItem.add(new EntryItem("어린이날", "2015.05.05 (화)", true));
+        mItem.add(new EntryItem("꿈끼 탐색 주간", "2015.05.06 (수)"));
+        mItem.add(new EntryItem("꿈끼 탐색 주간", "2015.05.07 (목)"));
+        mItem.add(new EntryItem("꿈끼 탐색 주간", "2015.05.08 (금)"));
+        mItem.add(new EntryItem("교내 체육대회 (1,2학년)", "2015.05.15 (금)"));
+        mItem.add(new EntryItem("현장 학습 (3학년)", "2015.05.15 (금)"));
+        mItem.add(new EntryItem("석가탄신일", "2015.05.25 (월)"));
+
+        mItem.add(new SectionItem("2015년 6월 일정"));
+        mItem.add(new EntryItem("전국 연합 학력 평가 (1,2학년)", "2015.06.04 (목)"));
+        mItem.add(new EntryItem("대 수능 모의 평가 (3학년)", "2015.06.04 (목)"));
+        mItem.add(new EntryItem("개교기념일", "2015.06.05 (금)", true));
+        mItem.add(new EntryItem("현충일", "2015.06.06 (토)", true));
+        mItem.add(new EntryItem("국가 수준 학업 성취도 평가 (2학년)", "2015.06.23 (화)"));
+
+        mItem.add(new SectionItem("2015년 7월 일정"));
+        mItem.add(new EntryItem("1학기 2회고사", "2015.06.29 (월)"));
+        mItem.add(new EntryItem("1학기 2회고사", "2015.06.30 (화)"));
+        mItem.add(new EntryItem("1학기 2회고사", "2015.07.01 (수)"));
+        mItem.add(new EntryItem("1학기 2회고사", "2015.07.02 (목)"));
+        mItem.add(new EntryItem("전국 연합 학력 평가 (3학년)", "2015.07.09 (목)"));
+        mItem.add(new EntryItem("여름방학식", "2015.07.17 (금)", true));
+
+        mItem.add(new SectionItem("2015년 8월 일정"));
+        mItem.add(new EntryItem("개학식", "2015.08.05 (수)"));
+
+        mItem.add(new SectionItem("2015년 9월 일정"));
+        mItem.add(new EntryItem("전국 연합 학력 평가 (1,2학년)", "2015.09.02 (수)"));
+        mItem.add(new EntryItem("대 수능 모의 평가 (3학년)", "2015.09.02 (수)"));
+        mItem.add(new EntryItem("영어 듣기 평가", "2015.09.15 (화)"));
+        mItem.add(new EntryItem("영어 듣기 평가", "2015.09.16 (수)"));
+        mItem.add(new EntryItem("영어 듣기 평가", "2015.09.17 (목)"));
+        mItem.add(new EntryItem("추석 연휴", "2015.9.28 (월)", true));
+        mItem.add(new EntryItem("추석 연휴", "2015.9.29 (화)", true));
+
+        mItem.add(new SectionItem("2015년 10월 일정"));
+        mItem.add(new EntryItem("2학기 1회고사", "2015.09.30 (수)"));
+        mItem.add(new EntryItem("2학기 1회고사", "2015.10.01 (목)"));
+        mItem.add(new EntryItem("2학기 1회고사", "2015.10.02 (금)"));
+        mItem.add(new EntryItem("2학기 1회고사", "2015.10.05 (월)"));
+        mItem.add(new EntryItem("한글날", "2015.10.09 (금)", true));
+        mItem.add(new EntryItem("전국 연합 학력 평가 (3학년)", "2015.10.13 (화)"));
+
+        mItem.add(new SectionItem("2015년 11월 일정"));
+        mItem.add(new EntryItem("대학 수학 능력 시험", "2015.11.12 (목)", true));
+        mItem.add(new EntryItem("2학기 2회고사 (3학년)", "2015.11.16 (월)"));
+        mItem.add(new EntryItem("2학기 2회고사 (3학년)", "2015.11.17 (화)"));
+        mItem.add(new EntryItem("2학기 2회고사 (3학년)", "2015.11.18 (수)"));
+        mItem.add(new EntryItem("2학기 2회고사 (3학년)", "2015.11.19 (목)"));
+        mItem.add(new EntryItem("전국 연합 학력 평가 (1,2학년)", "2015.11.17 (화)"));
+
+        mItem.add(new SectionItem("2015년 12월 일정"));
+        mItem.add(new EntryItem("2학기 2회고사 (1,2학년)", "2015.11.30 (월)"));
+        mItem.add(new EntryItem("2학기 2회고사 (1,2학년)", "2015.12.01 (화)"));
+        mItem.add(new EntryItem("2학기 2회고사 (1,2학년)", "2015.12.02 (수)"));
+        mItem.add(new EntryItem("2학기 2회고사 (1,2학년)", "2015.12.03 (목)"));
+        mItem.add(new EntryItem("졸업 사정회 (3학년)", "2015.12.07 (월)"));
+        mItem.add(new EntryItem("한마음 으뜸제", "2015.12.16 (수)"));
+        mItem.add(new EntryItem("겨울 방학식", "2015.12.18 (금)"));
+
+        mItem.add(new SectionItem("2016년 1월 일정"));
+        mItem.add(new EntryItem("신정", "2016.01.01 (금)"));
+
+        mItem.add(new SectionItem("2016년 2월 일정"));
+        mItem.add(new EntryItem("개학식", "2016.02.01 (월)"));
+        mItem.add(new EntryItem("졸업식/종업식", "2016.02.04 (목)"));
 
         EntryAdapter adapter = new EntryAdapter(this, mItem);
         mListview.setAdapter(adapter);
